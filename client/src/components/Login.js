728a40881399118c1250b2e24e93a3218558f343
sittora <sittora@gmail.com>
2021-10-27 13:55:00 -0400

    CSS style changed, working on adjusting things to center

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -32,7 +32,7 @@ return (
     <div className="login">
         <Redirect to="/" />
     <form className="w-2/3 bg-white p-8 max-w-md space-y-4" onSubmit={handleSubmit}>
-        <h1 className="text-2xl text-center font-bold mb-2">Log In</h1>
+        <h1 className="login-text">Log In</h1>
         <p>
             <label 
             className="block text-lg font-semibold"
@@ -45,7 +45,7 @@ return (
                 name="username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                className="w-full p-2 border"
+                className="username-box"
             />
         </p>
         <p>
@@ -60,12 +60,12 @@ return (
             name=""
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-2 border"
+            className="password-box"
         />
         </p>
-        <p><button className="w-full bg-green-500 py-2 mt-4" type="submit">Log In</button></p>
-        <p className="text-center">-- or --</p>
-        <p className="text-center"><Link className="py-4 px-6" to="/signup">Sign Up</Link></p>
+        <p><button className="login-button" type="submit">Log In</button></p>
+        <p className="or-text">-- or --</p>
+        <p className="text-center"><Link className="signup-button" to="/signup">Sign Up</Link></p>
     </form>
     </div>
 )
